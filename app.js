0fde12f936df0473b9b929bbaf2e445ebf0e4ad0
yuhsuan <yuhsuantsaii@gmail.com>
2023-04-14 15:05:48 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,181 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+//getContext()method會回傳一個canvas的drawing context
+//drawing context可以用來在canvas內畫圖
+const unit = 20;
+const row = canvas.height / unit;
+const column = canvas.width / unit;
+
+let snake = []; //array中的每個元素，都是一個物件
+function createSnake() {
+  snake[0] = {
+    x: 80,
+    y: 0,
+  };
+  snake[1] = {
+    x: 60,
+    y: 0,
+  };
+  snake[2] = {
+    x: 40,
+    y: 0,
+  };
+  snake[3] = {
+    x: 20,
+    y: 0,
+  };
+}
+//物件的工作是，儲存身體的x y座標
+
+class Fruit {
+  constructor() {
+    this.x = Math.floor(Math.random() * column) * unit;
+    this.y = Math.floor(Math.random() * row) * unit;
+  }
+  drawFruit() {
+    ctx.fillStyle = "Khaki";
+    ctx.fillRect(this.x, this.y, unit, unit);
+  }
+  pickALocation() {
+    //要看蛇跟果實的位置有沒有重疊，有的話選新位置
+
+    let new_x, new_y;
+    let overlapping = false;
+
+    function checkoverlap(new_x, new_y) {
+      for (let i = 0; i < snake.length; i++) {
+        if (new_x == snake[i].x && new_y == snake[i].y) {
+          overlapping = true;
+          return;
+        } else {
+          overlapping = false;
+        }
+      }
+    }
+    do {
+      new_x = Math.floor(Math.random() * column) * unit;
+      new_y = Math.floor(Math.random() * row) * unit;
+      checkoverlap(new_x, new_y);
+    } while (overlapping);
+    this.x = new_x;
+    this.y = new_y;
+  }
+}
+
+//初始設定
+createSnake();
+let myFruit = new Fruit();
+window.addEventListener("keydown", changeDirection);
+let d = "Right";
+//轉向
+function changeDirection(e) {
+  //   console.log(e);
+  if (e.key == "ArrowRight" && d != "Left") {
+    d = "Right";
+  } else if (e.key == "ArrowLeft" && d != "Right") {
+    d = "Left";
+  } else if (e.key == "ArrowUp" && d != "Down") {
+    d = "Up";
+  } else if (e.key == "ArrowDown" && d != "Up") {
+    d = "Down";
+  }
+
+  //每次按下上下左右鍵之後，在下一幀被畫出來之前，不接受任何keydown事件
+  //這樣可以防止連續案件導致蛇在邏輯上自殺
+  window.removeEventListener("keydown", changeDirection);
+}
+let highestScore;
+loadaHigherScore();
+let score = 0;
+document.getElementById("myScore").innerHTML = "遊戲分數：" + score;
+document.getElementById("myScore2").innerHTML = "最高分數：" + highestScore;
+
+function draw() {
+  //每次畫圖前，確認蛇有沒有咬到自己
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+      clearInterval(myGame);
+      alert("遊戲結束");
+      return;
+    }
+  }
+
+  //背景全設定為黑色
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  myFruit.drawFruit();
+  //畫出蛇
+  for (let i = 0; i < snake.length; i++) {
+    if (i == 0) {
+      ctx.fillStyle = "LightSteelBlue";
+    } else {
+      ctx.fillStyle = "LightBLue";
+    }
+    ctx.strokeStyle = "white";
+    //穿過牆
+    if (snake[i].x < 0) {
+      snake[i].x = canvas.width - unit;
+    }
+    if (snake[i].x >= canvas.width) {
+      snake[i].x = 0;
+    }
+    if (snake[i].y < 0) {
+      snake[i].y = canvas.height - unit;
+    }
+    if (snake[i].y >= canvas.height) {
+      snake[i].y = 0;
+    }
+
+    //(x,y,width,height)
+    ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
+    ctx.strokeRect(snake[i].x, snake[i].y, unit, unit);
+  }
+
+  //以目前的d變數，來決定蛇的下一幀數要放在哪個座標
+  let snakeX = snake[0].x; //snake is a object,but snake[0]is a number
+  let snakeY = snake[0].y;
+  if (d == "Left") {
+    snakeX -= unit;
+  } else if (d == "Up") {
+    snakeY -= unit;
+  } else if (d == "Down") {
+    snakeY += unit;
+  } else if (d == "Right") {
+    snakeX += unit;
+  }
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  //確認蛇是否吃到果實
+  if (snake[0].x == myFruit.x && snake[0].y == myFruit.y) {
+    myFruit.pickALocation();
+    score++;
+    setHighestScore(score);
+    document.getElementById("myScore").innerHTML = "遊戲分數：" + score;
+    document.getElementById("myScore2").innerHTML = "最高分數：" + highestScore;
+  } else {
+    snake.pop();
+  }
+  snake.unshift(newHead);
+  window.addEventListener("keydown", changeDirection);
+}
+
+let myGame = setInterval(draw, 100);
+
+function loadaHigherScore() {
+  if (localStorage.getItem("highestScore") == true) {
+    highestScore = 0;
+  } else {
+    highestScore = Number(localStorage.getItem("highestScore"));
+  }
+}
+
+function setHighestScore(score) {
+  if (score > highestScore) {
+    localStorage.setItem("highestScore", score);
+    highestScore = score;
+  }
+}
